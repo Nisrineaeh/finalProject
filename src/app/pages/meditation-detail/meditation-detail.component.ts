7dd84d5b20f64df6817d0e33ec43fff2aaa313bc
Nisrineaeh <nisrineaitelhaj@yahoo.fr>
2023-08-08 12:26:09 +0200

    keywords image ok

--- src/app/pages/meditation-detail/meditation-detail.component.ts
+++ src/app/pages/meditation-detail/meditation-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { concatMap, interval, take, timer } from 'rxjs';
 import { Technique } from 'src/app/models/technique';
 import { TechSelectService } from 'src/app/services/tech-select.service';
 import { TechniqueService } from 'src/app/services/technique.service';
@@ -11,6 +12,8 @@ import { TechniqueService } from 'src/app/services/technique.service';
 })
 export class MeditationDetailComponent implements OnInit{
   @Input() meditations!: Technique;
+  keywords: string[] = [];
+  displayedWords: string[]=[]
 
   constructor(
     private route: ActivatedRoute,
@@ -22,6 +25,8 @@ export class MeditationDetailComponent implements OnInit{
     if (meditationId !== null){
       this.meditationService.getMeditationById(meditationId).subscribe((donnee)=>{
         this.meditations= donnee;
+        this.keywords = this.meditations.mot_clefs;
+        this.displayWords();
       })
     }
   }
@@ -41,8 +46,15 @@ export class MeditationDetailComponent implements OnInit{
     // Réinitialiser les valeurs du chronomètre si nécessaire
   }
 
-  //methode pour mot clefs intervalle 5secondes
-
+  displayWords():void{
+    interval(10000)
+    .pipe(
+      take(this.meditations.mot_clefs.length) // Display 1 à 1
+    )
+      .subscribe(index => {
+        this.displayedWords.push(this.meditations.mot_clefs[index]);
+      });
+  }
 
 }
 
