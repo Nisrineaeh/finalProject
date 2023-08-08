aa898fa7882491d5fc4a2f9a309f6bdf4b3a308a
Nisrineaeh <nisrineaitelhaj@yahoo.fr>
2023-08-08 14:31:24 +0200

    2nd essaye recup connexion

--- src/app/pages/connexion/connexion.component.ts
+++ src/app/pages/connexion/connexion.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UtilisateursService } from 'src/app/services/utilisateurs.service';
 
 @Component({
   selector: 'app-connexion',
   templateUrl: './connexion.component.html',
   styleUrls: ['./connexion.component.css']
 })
-export class ConnexionComponent {
+export class ConnexionComponent implements OnInit {
+
+  connexionForm!: FormGroup;
+
+  constructor(private utilisateurService: UtilisateursService){}
+  ngOnInit(): void {
+    this.connexionForm=new FormGroup({
+       email: new FormControl('', [Validators.required, Validators.email]),
+       mdp: new FormControl('', [Validators.required])
+     });
+  }
+  
+
+
+  async onSubmit() {
+    if (this.connexionForm.valid) {
+      const email = this.connexionForm.value.email;
+      const motDePasse = this.connexionForm.value.mdp;
+
+    //   try {
+    //     const estConnecte = await this.utilisateurService.verifierConnexion(email, motDePasse);
+    //     if (estConnecte) {
+    //       alert('Vous êtes connecté !');
+    //     } else {
+    //       alert('Informations de connexion incorrectes. Veuillez réessayer !');
+    //     }
+    //   } catch (error) {
+    //     console.error('Une erreur est survenue lors de la vérification de la connexion :', error);
+    //     alert('Une erreur est survenue lors de la vérification de la connexion. Veuillez réessayer plus tard.');
+    //   }
+    // }
+  }
+  }
+
+
+
+
+
+
+
 
 }
--- src/app/services/utilisateurs.service.ts
+++ src/app/services/utilisateurs.service.ts
@@ -4,6 +4,10 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Utilisateur } from '../models/utilisateur';
 
+import * as bcrypt from 'bcrypt';
+
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -18,7 +22,10 @@ export class UtilisateursService {
 
   inscription(utilisateur: Utilisateur): Observable<Utilisateur>{
     return this.http.post<Utilisateur>(this.bddUrl, utilisateur);
+    
   }
+
+  
 
   majUtilisateur(id: number, utilisateur: Utilisateur): Observable<Utilisateur>{
     return this.http.patch<Utilisateur>(this.bddUrl, utilisateur);
@@ -27,4 +34,19 @@ export class UtilisateursService {
   suppUtilisateur (id:number): Observable<void>{
     return this.http.delete<void>(this.bddUrl);
   }
+
+  // async verifierConnexion(email: string, motDePasse: string): Promise<boolean> {
+  //   // Récupérer l'utilisateur par email depuis l'API
+  //   const utilisateur = await this.http.get<Utilisateur>(`${this.bddUrl}?email=${email}`).toPromise();
+
+  //   if (utilisateur) {
+  //     // Comparer le mot de passe fourni avec le mot de passe haché dans la base de données
+  //     const match = await bcrypt.compare(motDePasse, utilisateur.mot_de_passe);
+  //     return match;
+  //   }
+
+  //   return false; // Aucun utilisateur trouvé
+  // }
+
+  
 }
